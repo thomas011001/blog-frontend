67e4bd9cfe1637c4f8ba27ed920b75968b124ce8
thomas011001 <thomasyacoub321@gmail.com>
2025-09-20 03:05:02 +0300

    Wrap application in AuthProvider and Toast components for context and notifications

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./colors.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import Toast from "./components/Toast.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <AuthProvider>
+    <Toast>
+      <RouterProvider router={router} />
+    </Toast>
+  </AuthProvider>
 );
