c660a75b42b108848db364a0660188df87f58be1
thomas011001 <thomasyacoub321@gmail.com>
2025-09-26 14:07:49 +0300

    Add routing for Home and Post components within a new Layout. Update routes configuration to include nested routes for better structure.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./routes/Login";
 import SignUp from "./routes/SignUp";
+import Home from "./routes/Home";
+import Layout from "./routes/Layout.jsx";
+import Post from "./routes/Post.jsx";
 
 const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/posts/:id", element: <Post /> },
+    ],
+  },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <SignUp /> },
 ]);
