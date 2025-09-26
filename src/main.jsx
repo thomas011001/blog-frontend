bd1178613df1474ba6016a6b5ab39140e2c6c2a1
thomas011001 <thomasyacoub321@gmail.com>
2025-09-26 14:07:54 +0300

    Replace Toast component with Toaster from react-hot-toast for improved notification handling in main application render.

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./colors.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import Toast from "./components/Toast.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <Toast>
-      <RouterProvider router={router} />
-    </Toast>
+    <Toaster />
+    <RouterProvider router={router} />
   </AuthProvider>
 );
