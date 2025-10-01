f44c7e7539c0248c2209886bae812ffd771a4667
thomas011001 <thomasyacoub321@gmail.com>
2025-10-01 19:49:58 +0300

    Add route for New component to enable article creation

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import SignUp from "./routes/SignUp";
 import Home from "./routes/Home";
 import Layout from "./routes/Layout.jsx";
 import Post from "./routes/Post.jsx";
+import New from "./routes/New.jsx";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/posts/:id", element: <Post /> },
+      { path: "new", element: <New /> },
     ],
   },
   { path: "/login", element: <Login /> },
